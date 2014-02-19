c714d3b2f79646dc26be7d5dc1af2fad82273b8d
Frank Robert Anderson <frob@249517.no-reply.drupal.org>
2014-02-18 23:35:05 -0800

    initial commit

---
+++ app/init.js
@@ -0,0 +1,19 @@
+require.config({
+  urlArgs: "bust=" + (new Date()).getTime(),  //Cache buster
+  paths: {
+    "jquery": "https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min",
+    "underscore": "vendor/underscore",
+    "backbone": "vendor/backbone/backbone"
+  }
+});
+
+require(["jquery", "underscore", "backbone"], function($, _, Backbone) {
+  console.log("Test output");
+  console.log("$: " + typeof $);
+  console.log("_: " + typeof _);
+  console.log("Backbone: " + typeof Backbone);
+});
+
+require(['jquery'], function($) {
+  $('body').append("<h1>Hello World</h1>");
+})
